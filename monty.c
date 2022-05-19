e331c366976e4b2fba0cb94f56bc96fd2eb0a9d7
TeoVH <villadamateo64@gmail.com>
2022-05-19 12:18:25 -0500

    Betty corrections

--- monty.c
+++ monty.c
@@ -1,48 +1,52 @@
 #include "monty.h"
 
-char* token = NULL;
+/**
+ * main - entry into interpreter
+ * @argc: argc counter
+ * @argv: arguments
+ * Return: 0 on success
+ */
 
 int main(int argc, char **argv)
 {
-    FILE *fptr;
-    char *line = NULL;
-    size_t len = 0;
-    stack_t *stack = NULL;
-    int line_number = 1;
+	FILE *fptr;
+	char *line, *token = NULL, NULL;
+	size_t len = 0;
+	stack_t *stack = NULL;
+	int line_number = 1;
 
-    if (argc != 2)
-    {
-        fprintf(stderr, "Usage: monty file\n");
-        exit(EXIT_FAILURE);
-    }
-    fptr = fopen(argv[1], "r");
-    if (fptr == NULL)
-    {
-        fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
-        exit(EXIT_FAILURE);}
-    while (getline(&line, &len, fptr) != -1)
-    {
-        token = strtok(line, " \n\t");
-        if (token == NULL)
-        {
-            line_number++;
-            continue;
-        }
-        if (strcmp(token, "push") == 0)
-        {
-            token = strtok(NULL, " \n\t");
-            if (token == NULL || IsNumb(token) == 0)
-            {
-                fprintf(stderr, "L%d: usage: push integer\n", line_number);
-                exit(EXIT_FAILURE);
-            }
-            push(&stack, line_number);}
-        else
-        {
-            pick_func(&stack, line_number);
-        }
-        line_number++; }
-        free(line);
-        fclose(fptr);
-        return (0);
-    }
+	if (argc != 2)
+	{
+		fprintf(stderr, "Usage: monty file\n");
+		exit(EXIT_FAILURE);
+	}
+	fptr = fopen(argv[1], "r");
+	if (fptr == NULL)
+	{
+		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
+		exit(EXIT_FAILURE);
+	}
+	while (getline(&line, &len, fptr) != -1)
+	{
+		token = strtok(line, " \n\t");
+		if (token == NULL)
+		{
+			line_number++;
+			continue;
+		}
+		if (strcmp(token, "push") == 0)
+		{
+			token = strtok(NULL, " \n\t");
+			if (token == NULL || IsNumb(token) == 0)
+			{
+				fprintf(stderr, "L%d: usage: push integer\n", line_number);
+				exit(EXIT_FAILURE);
+			}
+			push(&stack, line_number); }
+		else
+			pick_func(&stack, line_number);
+		line_number++; }
+	free(line);
+	fclose(fptr);
+	return (0);
+}
