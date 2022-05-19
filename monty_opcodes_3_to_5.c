68641b76ca8096d15797990606179cddacc70892
TeoVH <villadamateo64@gmail.com>
2022-05-19 18:12:22 -0500

    Nop added

---
+++ monty_opcodes_3_to_5.c
@@ -0,0 +1,16 @@
+#include "monty.h"
+
+/**
+ * nop - doesn’t do anything
+ *
+ * @stack: the double pointer
+ * @line_number: read line
+ */
+
+void nop(stack_t **stack, unsigned int line_number)
+{
+	(void) stack;
+	(void) line_number;
+
+	return;
+}
--- pick_func.c
+++ pick_func.c
@@ -14,6 +14,7 @@ void pick_func(stack_t **stack, unsigned int line_number)
 		{"pall", pall},
 		{"pint", pint},
 		{"pop", pop},
+		{"nop", nop},
 		{NULL, NULL}};
 
 	int i = 0;
