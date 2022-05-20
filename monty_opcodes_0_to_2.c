0ebdfb8b03dcc4cd37c60d496a7a0b497d59c972
felipeholbi <4758@holbertonschool.com>
2022-05-19 20:10:42 -0500

    fix_pop

--- monty_opcodes_0_to_2.c
+++ monty_opcodes_0_to_2.c
@@ -77,7 +77,7 @@ void pop(stack_t **stack, unsigned int line_number)
 {
 	if (stack == NULL || *stack == NULL)
 	{
-		printf("L%u: can't pop an empty stack\n", line_number);
+		printf(stderr, "L%u: can't pop an empty stack\n", line_number);
 		free_stack(*stack);
 		exit(EXIT_FAILURE);
 	}
